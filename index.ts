6c1946500e7a5b7534a5eefe6799b6cffa172982
Mehak Noor Mohammad <159150549+Mehakzeeshan@users.noreply.github.com>
2024-04-15 23:27:05 +0500

    final changes

--- index.ts
+++ index.ts
@@ -31,8 +31,8 @@ let user_answer = await inquirer.prompt([
   },
 ]);
 
-let fromAmount = currency[user_answer.from]
-let toAmount = currency[user_answer.to]
+let fromAmount = currency[user_answer.from];
+let toAmount = currency[user_answer.to];
 let amount = user_answer.amount
 let baseAmount = amount / fromAmount
 let convertedAmount = baseAmount * toAmount
